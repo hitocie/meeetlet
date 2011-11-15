33de61344725d84fc73272211932482c6de34caa
hokada <hitoshi.dasu@gmail.com>
2011-11-16 00:50:52 +0900

    Fixed a little bit.

--- war/mobile/list/event.js
+++ war/mobile/list/event.js
@@ -4,6 +4,8 @@ $('#list-event').live('pageshow', function(event, ui) {
 	var id = v.id;
 	var kind = v.kind;
 	$('#event-id').text('イベントIDは「' + id + '」。');
+	// TODO: イベント参加済みなら、参加取り消しボタンへ
+	// TODO: 本日参加なら、予定通り行けそうにないかもボタンや、今どこボタン／現在状況を追加
 });
 
 $('#list-event').live('pagehide', function(event, ui) {
