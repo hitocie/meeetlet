672cda77b11d2f0992dc94fce9e9623e54fb93e9
Katsushi Fukui <kfukui@fukui-katsushi-no-MacBook-Pro.local>
2011-11-29 00:47:48 +0900

    Update mobile gui.

---
+++ war/mobile2/newArrange.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+var dateList = [];
+
+$(document).ready (function() {
+	$("#append").live("click", function(event, ui) {
+		var adate = $("#cal").val();
+		if (adate == "") return false;
+		if (dateList.indexOf(adate) >= 0) {
+			return false;
+		}
+		dateList.push(adate);
+		$("#datelist").append($('<li>'+adate+'</li>'));
+		$("#datelist").listview('refresh');
+	});
+});
