c456a4b969e6091218648cd60462c3cdaaccb362
Katsushi Fukui <kfukui@fukui-katsushi-no-MacBook-Pro.local>
2012-01-23 00:25:58 +0900

    Updated Web UI (not completed...)

--- web/src/main.js
+++ web/src/main.js
@@ -1,25 +1,4 @@
-$(function() {
-  $('#search').button();
-  $('#search').click(function(){
-    alert("検索");
-  });
-  
-  $('#prevbutton').button({icons: {primary: "ui-icon-circle-triangle-w"}});
-  $('#nextbutton').button({icons: {secondary: "ui-icon-circle-triangle-e"}});
-  
-  var dates = $( "#fromdate, #todate" ).datepicker({
-    dateFormat: 'yy/mm/dd',
-    numberOfMonths: 1,
-    onSelect: function( selectedDate ) {
-      var option = this.id == "fromdate" ? "minDate" : "maxDate",
-        instance = $( this ).data( "datepicker" ),
-        date = $.datepicker.parseDate(
-          instance.settings.dateFormat || $.datepicker._defaults.dateFormat,
-            selectedDate, instance.settings );
-      dates.not( this ).datepicker( "option", option, date );
-    }
-  });
-
+$(function() {   
   // portlet
   $( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
     .find( ".portlet-header" )
@@ -28,65 +7,126 @@ $(function() {
     .end()
     .find( ".portlet-content" )
     .hide();
-    
-  $('.detailbutton').button({icons: {secondary: "ui-icon-circle-triangle-e"}});
 
   $( ".portlet-header .ui-icon" ).click(function() {
-    $( this ).toggleClass( "ui-icon-plusthick" ).toggleClass( "ui-icon-minusthick" );
-    $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
-  });
-
-  $( ".column" ).disableSelection();
+	    $( this ).toggleClass( "ui-icon-plusthick" ).toggleClass( "ui-icon-minusthick" );
+	    $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
+	  });
   
-  // dialog
-  $( "#detaildialog" ).dialog({
-			autoOpen: false,
-			height: 500,
-			width: 500,
-			modal: true,
-			buttons: {
-				"参加する！": function() {
-      				alert("参加します");
-				},
-				Cancel: function() {
-					$( this ).dialog( "close" );
-				}
-			},
-			close: function() {
-				allFields.val( "" ).removeClass( "ui-state-error" );
-			}
-  });
-  $( ".detailbutton" ).button().click(function() {
-    $( "#detaildialog" ).dialog( "open" );
-  });
+  $( ".column" ).disableSelection();
   
   // menu
   var timeout = 200;
-var closetimer = 0;
-var ddmenuitem = 0;
+  var closetimer = 0;
+  var ddmenuitem = 0;
 
-function umenu_open()
-{  umenu_canceltimer();
-   umenu_close();
-   ddmenuitem = $(this).find('ul').css('visibility', 'visible');}
+  function umenu_open() {
+	  umenu_canceltimer();
+	  umenu_close();
+	  ddmenuitem = $(this).find('ul').css('visibility', 'visible');
+  }
 
-function umenu_close()
-{  if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+  function umenu_close() {
+	  if (ddmenuitem) ddmenuitem.css('visibility', 'hidden');
+  }
 
-function umenu_timer()
-{  closetimer = window.setTimeout(umenu_close, timeout);}
+  function umenu_timer() {
+	  closetimer = window.setTimeout(umenu_close, timeout);
+  }
 
-function umenu_canceltimer()
-{  if(closetimer)
-   {  window.clearTimeout(closetimer);
-      closetimer = null;}}
+  function umenu_canceltimer() {
+	  if(closetimer) {
+		  window.clearTimeout(closetimer);
+		  closetimer = null;
+	  }
+  }
 
-$(document).ready(function()
-{
-   $('#umenu > li').bind('mouseover', umenu_open);
-   $('#umenu > li').bind('mouseout',  umenu_timer);
-	});
+  $(document).ready(function() {
+	  $('#umenu > li').bind('mouseover', umenu_open);
+	  $('#umenu > li').bind('mouseout',  umenu_timer);
+  });
 
-document.onclick = umenu_close;
+  document.onclick = umenu_close;
+  
+  // button
+  //// event button
+  function showEventDetail(eid) {
+	  $('.showNow')
+	  	.removeClass('showNow')
+	  	.fadeOut(100, function() {$('.event-detail').addClass('showNow').fadeIn(300, null);});
+	  return false;
+  }
+  $('.eventDetailButton')
+  	.button({icons: {secondary: "ui-icon-circle-triangle-e"}})
+  	.click(function(){showEventDetail(1)});
+  
+  ////arrange button
+  function showArrangeDetail(eid) {
+	  $('.showNow')
+	  	.removeClass('showNow')
+	  	.fadeOut(100, function() {$('.arrange-detail').addClass('showNow').fadeIn(300, null);});
+	  return false
+  }
+  $('.arrangeDetailButton')
+  	.button({icons: {secondary: "ui-icon-circle-triangle-e"}})
+  	.click(function(){showArrangeDetail(1)});
+  
+  //// public button
+  function showPublicDetail(eid) {
+	  $('.showNow')
+	  	.removeClass('showNow')
+	  	.fadeOut(100, function() {$('.public-detail').addClass('showNow').fadeIn(300, null);});
+	  return false;
+  }
+  $('.publicDetailButton')
+  	.button({icons: {secondary: "ui-icon-circle-triangle-e"}})
+  	.click(function(){showPublicDetail(1)});
+  
+  //// attend button
+  $('.attendbutton').button({icons: {secondary: "ui-icon-star"}});
+  
+  //// arrange button
+  function arrangeEvent(eid) {
+	  $('.showNow')
+	  	.removeClass('showNow')
+	  	.fadeOut(100, function() {$('.arrangeEvent').addClass('showNow').fadeIn(300, null);})
+  }
+  $('.arrangebutton')
+  	.button({icons: {secondary: "ui-icon-star"}})
+  	.click(function(){arrangeEvent(1);});
 
+  //// absent button
+  function absentEvent(eid) {
+	 alert("欠席されたイベントを欠席イベントにします。"); 
+  }
+  $('.absentbutton')
+  	.button({icons: {secondary: "ui-icon-closethick"}})
+  	.click(function(){absentEvent(1);});
+
+  //// invited friends button
+  function showFriendsList(eid) {
+	  alert("show friends!");
+  }
+  $('.invitedFriendsButton')
+  	.button({icons: {}})
+  	.click(function(){showFriendsList(1);});
+  	
+  //// cancel button
+  function cancelArrange() {
+	  $('.showNow')
+	  	.removeClass('showNow')
+	  	.fadeOut(100, function() {$('.arrange-detail').addClass('showNow').fadeIn(300, null);});
+  }
+  $('.cancelbutton')
+  	.button({icons: {secondary: "ui-icon-closethick"}})
+  	.click(function(){cancelArrange();});
+  
+  //// reply button
+  function replyArrange(eid) {
+	  alert("調整を反映しました。");
+  }
+  $('.replybutton')
+  	.button({icons: {secondary: "ui-icon-mail-open"}})
+  	.click(function(){replyArrange(1);});
+  
 });
--- web/src/new.js
+++ web/src/new.js
@@ -28,8 +28,45 @@ $(document).ready(function()
 
 document.onclick = umenu_close;
 
-// date Calender
-$("#dateCal").datepicker({minDate: new Date()});
+// date Calendar
+//// select Calendar type
+$('#cal').click(function(){
+	$('.showNow')
+		.removeClass('showNow')
+		.fadeOut(100, function() {$('#calCalendar').addClass('showNow').fadeIn(100, null);});
+});
+$('#multi').click(function(){
+	$('.showNow')
+		.removeClass('showNow')
+		.fadeOut(100, function() {$('#multiCalendar').addClass('showNow').fadeIn(100, null);});
+})
+$('#fromTo').click(function(){
+	$('.showNow')
+		.removeClass('showNow')
+		.fadeOut(100, function() {$('#fromToCalendar').addClass('showNow').fadeIn(100, null);});
+})
+//// calendar setting
+$(".dateCal").datepicker({minDate: new Date()});
+var dates = $( "#from, #to" ).datepicker({
+	onSelect: function( selectedDate ) {
+		var option = this.id == "from" ? "minDate" : "maxDate",
+			instance = $( this ).data( "datepicker" ),
+			date = $.datepicker.parseDate(
+				instance.settings.dateFormat ||
+				$.datepicker._defaults.dateFormat,
+				selectedDate, instance.settings );
+		dates.not( this ).datepicker( "option", option, date );
+	}
+});
+
+function addMultiCalendar() {
+	var txt=$("<p><input type='text' id='multiCal6' class='dateCal'></p>");
+	$("#mulitCalList").append(txt);
+	alert($("#mulitCalList").html());
+}
+$("#addCalendarButton")
+	.button({icons: {secondary: "ui-icon-circle-plus"}})
+	.click(function() {addMultiCalendar();});
 
 // drag and drop
 $('.fimg').draggable({
